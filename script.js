a0054b58691a18db37202d83652070c0af2b33cf
AzimAhmedBijapur <azeembijapur786@gmail.com>
2022-12-20 10:24:19 +0530

    Gui almost done

---
+++ script.js
@@ -0,0 +1,17 @@
+var controls=document.querySelector('.controls');
+
+// for(let i=0;i<5;i++){
+
+//     var div=document.createElement('div');
+
+//     div.classList.add('buttonRow');
+
+//     controls.appendChild(div);
+
+//     for(let j=0;j<5;j++){
+
+//         var buttonCol = document.createElement('div');
+//         buttonCol.classList.add('buttonCol');
+//         div.appendChild(buttonCol);
+//     }
+// }
